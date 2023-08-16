16b0ead70e381282be44843816606fedca1e3e9b
Syed Azaz Hussain Shah <azaz.hussain619@gmail.com>
2023-08-16 19:03:59 +0500

    Made ready for production

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('vendor/startbootstrap/assets/favicon.ico') }}"/>
+
     <!-- Fonts -->
     {{--    <link rel="dns-prefetch" href="//fonts.bunny.net">--}}
     {{--    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">--}}
